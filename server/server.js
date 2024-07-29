942743ef9f7f021800c5d99c0be03ab41122ecd3
harshittsaini <harshitsaini61@gmail.com>
2024-07-29 23:01:46 +0530

    bug fixes

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_URI)
     .then(()=>{
         //listen for request
         app.listen(process.env.PORT, ()=>{
-            console.log('connected to db and listening to port ', process.env.PORT)
+            console.log('connected to db and listening to port ', PORT)
         })
     })
     .catch((error) => {
@@ -31,11 +31,8 @@ app.use('/api/expenses', expensesRoute);
 const stockRoutes = require('./routes/stockRoutes');
 app.use('/api/stocks', stockRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// console.log('Clerk API Key:', process.env.CLERK_API_KEY);
-// console.log('Mongo URI:', process.env.MONGO_URI);
 
